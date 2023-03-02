84e11f4663314b344deb7a06338d05bd371852a2
Ryan Pope <rcpope7@gmail.com>
2023-03-02 01:02:31 -0500

    keys are a struct now. less spaghetti no regretti

--- server/main.go
+++ server/main.go
@@ -37,6 +37,46 @@ const (
 	CLIPBOARD_INACTIVE_COLOR      = "000000"
 )
 
+type Key struct {
+	// how many times in a row this key has been pressed
+	sequentialPressCount uint
+
+	// if this key has seen KEYDOWN but not KEYUP
+	isPressed bool
+
+	// KEYHOLD messages are not synced perfectly, and limited to one bit
+	// it cycles 0->F->0, so we keep this to keep a (mostly) accurate sum
+	lastDurationValue byte
+
+	// this is the sum, see above comment
+	pressDuration uint
+}
+
+func (k *Key) handle(i *Instruction) {
+	switch i.arg2 {
+	case KEYDOWN:
+		k.sequentialPressCount++
+		k.isPressed = true
+	case KEYHOLD:
+		var extra byte
+		if i.arg3 < k.lastDurationValue {
+			// if we rolled over to next cycle, account for that
+			extra = 0xF + 1
+		}
+		k.pressDuration += uint(i.arg3 + extra - k.lastDurationValue)
+	case KEYUP:
+		k.isPressed = false
+	default:
+		return
+	}
+}
+
+func (k *Key) reset() {
+	k.isPressed = false // in case we missed a KEYDOWN somehow
+	k.sequentialPressCount = 0
+	k.pressDuration = 0
+}
+
 type Instruction struct {
 	instructionCode byte
 	arg1            byte
@@ -47,7 +87,7 @@ type Instruction struct {
 
 func NewInstructionFromSerial(serialInput string) (*Instruction, bool, error) {
 	serialInput = strings.TrimSuffix(serialInput, "\r\n")
-	fmt.Printf("parsing: %s\n", serialInput)
+	// fmt.Printf("parsing: %s\n", serialInput)
 	var sanitizedInput string
 	if prefixIndex := strings.Index(serialInput, ARD_INSTRUCTION_PREFIX); prefixIndex == 0 {
 		sanitizedInput = serialInput[prefixIndex+ARD_INSTRUCTION_PREFIX_LENGTH : len(serialInput)-1]
@@ -91,6 +131,8 @@ var (
 	port             serial.Port
 	clipboardManager *ClipboardManager
 	spacingRegex     = regexp.MustCompile(`[^\S ]`)
+	keys             [NUM_KEYS]*Key
+	lastKeyIndex     = 0
 )
 
 func sendSerial(message []byte) error {
@@ -151,8 +193,9 @@ type Clipboard struct {
 }
 
 type ClipboardManager struct {
-	clipboards    [NUM_KEYS]*Clipboard
-	previousIndex int
+	clipboards          [NUM_KEYS]*Clipboard
+	previousIndex       int
+	previousUpdateIndex int
 }
 
 func NewClipboardManager(directory string) *ClipboardManager {
@@ -206,6 +249,7 @@ func (cm *ClipboardManager) setClip(index int) (string, error) {
 
 	cm.clipboards[index].value = clipboardContents
 	cm.previousIndex = index
+	cm.previousUpdateIndex = index
 	return clipboardContents, nil
 }
 
@@ -230,10 +274,12 @@ func (cm *ClipboardManager) getClip(index int) (string, error) {
 	}
 
 	cm.previousIndex = index
+	cm.previousUpdateIndex = index
 	return buf.String(), file.Close()
 }
 
 func (cm *ClipboardManager) hasClip(index int) bool {
+	cm.previousIndex = index
 	return cm.clipboards[index] != nil && cm.clipboards[index].value != ""
 }
 
@@ -241,6 +287,7 @@ func (cm *ClipboardManager) deleteClip(index int) error {
 	if cm.hasClip(index) {
 		cm.clipboards[index].value = ""
 		cm.previousIndex = index
+		cm.previousUpdateIndex = index
 		return os.Truncate(cm.clipboards[index].filepath, 0)
 	}
 	return fmt.Errorf("attempted to delete non-existent clipboard %d", index)
@@ -282,58 +329,74 @@ func openOrCreate(path string) (*os.File, bool, error) {
 func delegate(i *Instruction) error {
 	switch i.instructionCode {
 	case EVENT:
-		keyIndex := int(i.arg1)
-		setOledText(0, 1, fmt.Sprintf("clipboard %d:", keyIndex))
-		err := clearOledLines(1, 0)
-		if err != nil {
-			return err
-		}
 		switch i.arg2 {
-		case KEYUP:
+		case KEYUP, KEYDOWN, KEYHOLD:
 			{
-				holdDurationMs := int(i.arg3) * 100
-				if holdDurationMs < 1000 {
+				keyIndex := int(i.arg1)
+				if keyIndex != lastKeyIndex {
+					keys[lastKeyIndex].reset()
+				}
+				currKey := keys[keyIndex]
+				currKey.handle(i)
+				fmt.Println(currKey)
+				setOledText(0, 1, fmt.Sprintf("clipboard %d:", keyIndex))
+				holdDurationMs := currKey.pressDuration * 100
+				if !currKey.isPressed { // we have seen KEYUP
+					if holdDurationMs < 1000 {
+						if clipboardManager.hasClip(keyIndex) {
+							clip, err := clipboardManager.getClip(keyIndex)
+							if err != nil {
+								return err
+							}
 
-					if clipboardManager.hasClip(keyIndex) {
-						doubleTap := keyIndex == clipboardManager.previousIndex
-						clip, err := clipboardManager.getClip(keyIndex)
-						if err != nil {
-							return err
-						}
-						if doubleTap {
-							clipboard.WriteAll(clip)
-							setOledText(1, 0, "successfully copied!")
-							setOledText(3, 0, clip)
-							clipboardManager.previousIndex = -1
+							if currKey.sequentialPressCount == 1 {
+								err = clearOledLines(1, 0)
+								if err != nil {
+									return err
+								}
+								setOledText(2, 0, clip)
+							} else if currKey.sequentialPressCount == 2 {
+								err = clearOledLines(1, 0)
+								if err != nil {
+									return err
+								}
+								clipboard.WriteAll(clip)
+								setOledText(2, 0, "successfully copied!")
+								setOledText(4, 0, clip)
+								clipboardManager.previousUpdateIndex = -1
+							}
 						} else {
-							setOledText(3, 0, clip)
+							err := clearOledLines(1, 0)
+							if err != nil {
+								return err
+							}
+							setOledText(2, 0, "nothing here =(")
+							setOledText(4, 0, "Hold for 1s to copy")
+							setOledText(5, 0, "from host clipboard.")
 						}
-					} else {
-						setOledText(2, 0, "nothing here :(")
-						setOledText(4, 0, "Hold for 1s to copy")
-						setOledText(5, 0, "from host clipboard")
+					} else if holdDurationMs >= 1000 {
+						clearOledLines(1, 0)
+						if !clipboardManager.hasClip(keyIndex) {
+							clip, err := clipboardManager.setClip(keyIndex)
+							if err != nil {
+								return err
+							}
+							setOledText(2, 0, "set to:")
+							setOledText(4, 0, clip)
+						} else {
+							err := clipboardManager.deleteClip(keyIndex)
+							if err != nil {
+								return err
+							}
+							setOledText(1, 0, "deleted clip!")
+						}
+						currKey.reset()
 					}
-				} else if clipboardManager.previousIndex != keyIndex && holdDurationMs >= 1000 && !clipboardManager.hasClip(keyIndex) {
-					clip, err := clipboardManager.setClip(keyIndex)
-					if err != nil {
-						return err
-					}
-					setOledText(1, 0, "set to:")
-					setOledText(3, 0, clip)
 				}
-			}
-		case KEYHOLD:
-			holdDurationMs := int(i.arg3) * 100
-			if holdDurationMs >= 1000 && clipboardManager.hasClip(keyIndex) {
-				err := clipboardManager.deleteClip(keyIndex)
-				if err != nil {
-					return err
-				}
-				clearOled()
-				setOledText(1, 0, "deleted clip!")
+				clipboardManager.updateLEDs()
+				lastKeyIndex = keyIndex
 			}
 		}
-		clipboardManager.updateLEDs()
 	}
 	return nil
 }
@@ -356,6 +419,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	for i := 0; i < NUM_KEYS; i++ {
+		keys[i] = &Key{}
+	}
+
+	clearOled()
+	setOledText(0, 1, "-MULTICLIPBOARD MODE-")
+	setOledText(2, 0, "registered with host!")
+	setOledText(4, 0, "press a key to start.")
+	setOledText(6, 0, "          =)         ")
 
 	clipboardManager = NewClipboardManager(CLIP_DIRECTORY)
 	clipboardManager.updateLEDs()
